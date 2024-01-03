169cdf76afbbeaa6299c901396d44665ad2d5539
Kirills Galenko <kirillgalenko@gmail.com>
2024-01-03 18:31:00 +0200

    transaction card view built

--- Expense Tracker/View/Helpers/TransactionCardView.swift
+++ Expense Tracker/View/Helpers/TransactionCardView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 struct TransactionCardView: View {
     var transaction: Transaction
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:12){
+            Text("\(String(transaction.title.prefix(1)))")
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .frame(width: 45, height: 45)
+                .background(transaction.color.gradient, in: .circle)
+            
+            VStack(alignment: .leading, spacing: 4, content: {
+                Text(transaction.title)
+                    .foregroundStyle(Color.primary)
+                
+                Text(transaction.remarks)
+                    .font(.caption)
+                    .foregroundStyle(Color.primary.secondary)
+                
+                Text(format(date: transaction.dateAdded, format: "dd MMM yyyy"))
+                    .font(.caption2)
+                    .foregroundStyle(Color.primary.secondary)
+            })
+            .lineLimit(1)
+            .hSpacing(.leading)
+            
+            Text(currencyString(transaction.amount, allowedDigits: 2))
+                .fontWeight(.semibold)
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 10)
+        .background(.background, in: .rect(cornerRadius: 10))
     }
 }
 
