4e63617b5bb4bde0555410245a9a9f91a031a3a9
Kirills Galenko <kirillgalenko@gmail.com>
2024-01-06 19:57:38 +0200

    date filter pop up

---
+++ Expense Tracker/View/Helpers/DateFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  DateFilterView.swift
+//  Expense Tracker
+//
+//  Created by Kirills Galenko on 06/01/2024.
+//
+
+import SwiftUI
+
+struct DateFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateFilterView()
+}
